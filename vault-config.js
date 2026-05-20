a604de4da8349784cd73f71f1aee02722c0fd153
kzick001 <askozicki@gmail.com>
2026-05-20 15:41:55 -0500

    added snippets, removed duplicates from favorites bar

--- vault-config.js
+++ vault-config.js
@@ -4,11 +4,9 @@ window.KzickVaultManifest = [
   {
     category: "Nerd Stuff",
     links: [
-      { text: "Cloudflare Core", url: "https://dash.cloudflare.com" },
-      { text: "GitHub Repos", url: "https://github.com/kzick" },
-      { text: "Spotify", url: "https://open.spotify.com/" },
-      { text: "Google Drive", url: "https://drive.google.com/drive/folders/1sQ5Z4Lp_7fcM0W7W4effQdT0BLQWEuSm" },
-      { text: "Kzick Weather", url: "https://kzick001.github.io/dashboard/weather.html" },
+      { text: "Cloudflare", url: "https://dash.cloudflare.com" },
+      { text: "Code Snippet Library", url: "https://kzick001.github.io/fragment/" },
+      { text: "KzickGitHub Repos", url: "https://github.com/kzick" },
       { text: "UB Game", url: "https://kzick001.github.io/undead-barrage/ub-v12" },
       { text: "UB Masterfile Creator", url: "https://kzick001.github.io/undead-barrage/master-file-creator.html" },
       { text: "API Reference (MagTag)", url: "https://docs.circuitpython.org/projects/magtag/en/latest/api.html" },
